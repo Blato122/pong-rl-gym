079718fe9e640cdb655dda2058d4eeca5793fb4b
blato <blator02@gmail.com>
2024-10-16 21:36:37 +0200

    some new stuff (what?)

--- 3-actions-pong.py
+++ 3-actions-pong.py
@@ -11,13 +11,13 @@ batch_size = 10 # every how many episodes to do a param update?
 learning_rate = 1e-3 # changed from 1e-4
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
-resume = False # resume from previous checkpoint?
-render = False
+resume = True # resume from previous checkpoint?
+render = True
 
 # model initialization
 D = 80 * 80 # input dimensionality: 80x80 grid
 if resume:
-  model = pickle.load(open('something.p', 'rb'))
+  model = pickle.load(open('11800ep-6.1reward-86.5win-save.p', 'rb'))
 else:
   model = {}
   model['W1'] = np.random.randn(D,H) / np.sqrt(D) # "Xavier" initialization
